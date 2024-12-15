3b97dcea9c5f511161bddac6d9728ab4d1cfb455
Optimus <ijaolatoyosi03@gmail.com>
2024-12-15 10:39:12 +0100

    Created the login file

--- login.php
+++ login.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form method="POST" action="functions.php?action=login">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html
