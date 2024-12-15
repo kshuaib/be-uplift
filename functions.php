2f9e09bd10c37f29ecee8c8d97a10cd5ba4f4c8f
kshuaib <kshuaib.w@gmail.com>
2024-12-15 05:34:04 -0800

    functions

--- functions.php
+++ functions.php
@@ -1,1 +1,19 @@
+<?php
+require "conn.php";
 
+//Register function
+function reg() {
+    global $conn;
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $username = mysqli_real_escape_string($conn, $_POST["username"]);
+        $email = mysqli_real_escape_string($conn, $_POST["email"]);
+        $password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+        $sql = "INSERT INTO members (name, email, password) VALUES ('$name', '$email', '$password')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "Registration successful!";
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
