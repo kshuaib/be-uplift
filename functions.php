912670ef843b4ad37c618fb12cc1014b214c3bad
kshuaib <kshuaib.w@gmail.com>
2024-12-16 10:29:00 +0100

    login function

--- functions.php
+++ functions.php
@@ -17,3 +17,24 @@ function reg() {
         } else {
             echo "Error: " . mysqli_error($conn);
         }
+
+        // Login function
+        function login() {
+            global $conn;
+
+            if($_SERVER["REQUEST_METHOD"] === "POST") {
+                $username = mysqli_real_escape_string($conn, $_POST["username"]);
+                $password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+                $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    header("Location: success.php");
+                    exit();
+                } else {
+                    echo "Invalid username or passwprd.";
+                }
+            }
+        }
+        ?>
